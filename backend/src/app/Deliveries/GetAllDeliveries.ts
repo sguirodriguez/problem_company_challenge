0127b6fc3d651835a095010d085e5b45b8a3873e
Samuel Rodrigues <guilherme.engr@gmail.com>
2022-12-15 21:13:44 -0300

    adjust deliveries

--- backend/src/app/Deliveries/GetAllDeliveries.ts
+++ backend/src/app/Deliveries/GetAllDeliveries.ts
@@ -3,7 +3,7 @@ import { models } from "../../models";
 class GetAllDeliveries {
   async execute() {
     try {
-      const sellers = await models().Sellers.findAll();
+      const sellers = await models().Deliveries.findAll();
 
       return { status: 200, response: { data: sellers, error: null } };
     } catch (error) {
